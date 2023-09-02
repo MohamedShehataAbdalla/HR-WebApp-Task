4207309d24dd8a3c6a2fdc348b5221e04a0c4ef6
MohamedShehataAbdalla <moh.shehata143@gmail.com>
2023-09-02 18:42:40 +0300

    Build Project

---
+++ HRApp/Models/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace HRApp.Models
+{
+    public class Employee
+    {
+        
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Please Enter Name")]
+        [StringLength(20, MinimumLength =3, ErrorMessage ="Invalid Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Job Ttitle")]
+        public string JobTtitle { get; set; }
+        public string Department { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+
+        public bool IsActive { get; set; }
+
+    }
+}
